f7b90f384462c62608544ba065029c8955023751
Keith O'Brien <keitheobrien@gmail.com>
2018-01-21 10:49:07 -0500

    Removed some sources

--- expand-block-list.sh
+++ expand-block-list.sh
@@ -32,7 +32,6 @@ echo "https://adaway.org/hosts.txt" | sudo tee -a /etc/pihole/adlists.list > /de
 echo "https://v.firebog.net/hosts/AdguardDNS.txt" | sudo tee -a /etc/pihole/adlists.list > /dev/null
 echo "https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt" | sudo tee -a /etc/pihole/adlists.list > /dev/null
 echo "https://hosts-file.net/ad_servers.txt" | sudo tee -a /etc/pihole/adlists.list > /dev/null
-echo "https://v.firebog.net/hosts/Easylist.txt" | sudo tee -a /etc/pihole/adlists.list > /dev/null
 echo "https://raw.githubusercontent.com/StevenBlack/hosts/master/data/SpotifyAds/hosts" | sudo tee -a /etc/pihole/adlists.list > /dev/null
 echo "https://raw.githubusercontent.com/StevenBlack/hosts/master/data/UncheckyAds/hosts" | sudo tee -a /etc/pihole/adlists.list > /dev/null
 echo "https://v.firebog.net/hosts/Airelle-trc.txt" | sudo tee -a /etc/pihole/adlists.list > /dev/null
@@ -45,9 +44,6 @@ echo "https://v.firebog.net/hosts/static/SamsungSmart.txt" | sudo tee -a /etc/pi
 echo "https://v.firebog.net/hosts/Airelle-hrsk.txt" | sudo tee -a /etc/pihole/adlists.list > /dev/null
 echo "https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt" | sudo tee -a /etc/pihole/adlists.list > /dev/null
 echo "https://mirror1.malwaredomains.com/files/justdomains" | sudo tee -a /etc/pihole/adlists.list > /dev/null
-echo "https://hosts-file.net/exp.txt" | sudo tee -a /etc/pihole/adlists.list > /dev/null
-echo "https://hosts-file.net/emd.txt" | sudo tee -a /etc/pihole/adlists.list > /dev/null
-echo "https://hosts-file.net/psh.txt" | sudo tee -a /etc/pihole/adlists.list > /dev/null
 echo "https://mirror.cedia.org.ec/malwaredomains/immortal_domains.txt" | sudo tee -a /etc/pihole/adlists.list > /dev/null
 echo "https://www.malwaredomainlist.com/hostslist/hosts.txt" | sudo tee -a /etc/pihole/adlists.list > /dev/null
 echo "https://bitbucket.org/ethanr/dns-blacklists/raw/8575c9f96e5b4a1308f2f12394abd86d0927a4a0/bad_lists/Mandiant_APT1_Report_Appendix_D.txt" | sudo tee -a /etc/pihole/adlists.list > /dev/null
@@ -59,7 +55,5 @@ echo "https://ransomwaretracker.abuse.ch/downloads/RW_DOMBL.txt" | sudo tee -a /
 echo "https://v.firebog.net/hosts/Shalla-mal.txt" | sudo tee -a /etc/pihole/adlists.list > /dev/null
 echo "https://raw.githubusercontent.com/StevenBlack/hosts/master/data/add.Risk/hosts" | sudo tee -a /etc/pihole/adlists.list > /dev/null
 echo "https://zeustracker.abuse.ch/blocklist.php?download=domainblocklist" | sudo tee -a /etc/pihole/adlists.list > /dev/null
-echo "https://github.com/chadmayfield/pihole-blocklists/raw/master/lists/pi_blocklist_porn_all.list" | sudo tee -a /etc/pihole/adlists.list > /dev/null
-echo "https://raw.githubusercontent.com/chadmayfield/pihole-blocklists/master/lists/pi_blocklist_porn_top1m.list" | sudo tee -a /etc/pihole/adlists.list > /dev/null
 
 pihole -g
